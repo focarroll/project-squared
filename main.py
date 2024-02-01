631cf16e55e31beec8afa0d7c495faf6f48f1f17
pbbrausch <pbbrausch@gmail.com>
2024-02-01 10:27:16 -0500

    added categories

--- main.py
+++ main.py
@@ -4,39 +4,87 @@ import os
 # import your functions here (example):
 
 #    #folder  #file name        #function name
-from programs.calculator import calculate
-from programs.hello import hello
-from programs.words import word
-from programs.rpg import rpg
+from programs.utilities.calculator import calculator
+from programs.misc.hello import hello
+from programs.misc.words import word
+from programs.games.rpg import rpg
+
+#Catagories for programs:
+catagories = ["Miscellaneous",
+              "Utilities",
+              "Games"]
 
 #add your imported function here:
-programs = [hello, 
-            calculate,
-            word,
-            rpg]
+misc = [hello, 
+        word]
+
+utilities = [calculator]
+
+games = [rpg]
 
 def main():
     #clear screen
-    os.system('cls' if os.name == 'nt' else 'clear')
+    while (True):
+        # Choose Catagory
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-    print("Choose a program: \n")
+        print("Choose a catagory: \n")
 
-    #show all program options
-    for i, option in enumerate(programs):
-        print(f'{i+1}. {option.__name__}.py\n')
+        #show all catagory options
+        for i, option in enumerate(catagories):
+            print(f'{i+1}. {option}\n')
 
-    #validate input is in range
-    choice = 0
-    while choice > len(programs) or choice < 1:
-        try:
-            choice = int(input(f'(1-{len(programs)}): '))
-        except ValueError:
+        print(f'{len(catagories)+1}. Back\n')
+
+        #validate input is in range
+        catagory = get_input(catagories)
+        
+        # Choose Program
+        clear_screen()
+        
+        print("Choose a program: \n")
+
+        programs = None
+        match (catagory):
+            case 0:              
+                programs = misc
+            case 1:
+                programs = utilities
+            case 2:
+                programs = games
+
+        #show all programs options in catagory
+        for i, option in enumerate(programs):
+            print(f'{i+1}. {option.__name__}.py\n')
+        
+
+        #validate input is in range
+        program = get_input(programs)
+
+        if (program == len(programs)):
             continue
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+        clear_screen()
+
+        programs[program]()
+
+        return
+
+def get_input(options, back=False):
+    choice = -1
+    while choice > len(options) or choice < 0:
+        try:
+            choice = int(input(f'(1-{len(options)}): ')) - 1
+
+        except ValueError:
+            continue
     
-    #execute programs
-    programs[choice-1]()
+    
+    return choice
+        
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 if __name__ == "__main__":
     main()
--- programs/utilities/calculator.py
+++ programs/utilities/calculator.py
@@ -1,6 +1,6 @@
 # Pierce Brausch '25
 
-def calculate():
+def calculator():
     equation = input("Enter a mathmatical equation: (ex: 1 * 3 / 2): \n")
 
     print(f"The answer is {eval(equation)}")
